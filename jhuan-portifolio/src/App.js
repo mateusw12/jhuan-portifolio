f8a4ef0a083420e1fb28e44ad9ab1e08b90990e5
Mateus Walz <mateus.walz@gmail.com>
2023-04-27 14:50:42 -0300

    Implementa componente de cabeçalho (#2)

--- jhuan-portifolio/src/App.js
+++ jhuan-portifolio/src/App.js
@@ -1,8 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Header />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
