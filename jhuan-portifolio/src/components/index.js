69a3fc007799f41e14bb3ec6dd3816af602af217
Mateus Walz <mateus.walz@gmail.com>
2023-04-28 11:50:14 -0300

    Implementa redirecionamento para a página inicial caso a url for inválida (#12)

--- jhuan-portifolio/src/components/index.js
+++ jhuan-portifolio/src/components/index.js
@@ -2,6 +2,7 @@ export { default as About } from './About';
 export { default as Content } from './Content';
 export { default as Header } from './Header';
 export { default as Home } from './Home';
+export { default as NotFound } from './NotFound';
 export { default as Project } from './Project';
 export { default as ProjectCard } from './ProjectCard';
 export { default as Skills } from './Skills';
