82336e59ec59f98f504b006b12235a70f5821772
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-04-28 11:49:33 -0300

    Implementa redirecionamento para a página inicial caso a url for inválida

--- jhuan-portifolio/src/App.js
+++ jhuan-portifolio/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { About, Content, Home, Project, Skills } from "./components/index";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { About, Content, Home, NotFound, Project, Skills } from "./components/index";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -14,6 +14,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/project" element={<Project />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Content>
     </BrowserRouter>
